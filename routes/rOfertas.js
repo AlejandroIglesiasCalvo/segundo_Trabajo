d1d15063c6919307dd7e7eba8819c003cbc5a6e6
Alejandro Iglesias Calvo <uo24346@uniovi.es>
2019-04-23 14:24:43 +0200

    Ya funciona el formulario de agregar ofertas!

--- routes/rOfertas.js
+++ routes/rOfertas.js
@@ -1,12 +1,13 @@
-module.exports = function(app) {
+module.exports = function(app, swig) {
     app.get("/oferta", function(req, res) {
         var respuesta = swig.renderFile('views/agregar.html', {});
         res.send(respuesta);
     });
 
     app.post("/oferta", function(req, res) {
-        res.send("Canción agregada:"+req.body.nombre +"<br>"
-            +" genero :" +req.body.genero +"<br>"
+        res.send("Canción agregada:"+req.body.titulo +"<br>"
+            +" genero :" +req.body.descripcion +"<br>"
             +" precio: "+req.body.precio);
     });
+
 };
